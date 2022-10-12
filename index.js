b70e908400d5fffdbb0d4cf5f117ef665834b722
Michelle Too <mjxtcodes@gmail.com>
2022-10-12 10:57:19 -0700

    commit tree 733bbf8d03f567c17cd84cf69f290568e81fbfe8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1)
+}
